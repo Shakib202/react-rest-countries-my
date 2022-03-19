9f6489dbc01016031176e034eac0bb1f68b66aa7
Shakib202 <shakib2020360@gmail.com>
2022-03-20 02:13:21 +0600

    fourth-commit

--- src/components/country/Country.js
+++ src/components/country/Country.js
@@ -2,9 +2,8 @@ import React from 'react';
 import './Country.css'
 const Country = (props) =>{
   const {name, population, region, area, flags} = props.country;
-  // console.log(props.country);
   return (
-    <div className='country'>
+    <div className='country bg-info'>
       <h1>Country Name: {name.common}</h1>
       <img src={flags.png} alt="" />
       <p>Country Population: {population}</p>
